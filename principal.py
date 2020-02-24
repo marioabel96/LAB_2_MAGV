ca2e2cc79e60f350fab98ccae4de62bd447965d2
marioabel96 <31260739+marioabel96@users.noreply.github.com>
2020-02-24 08:32:06 -0600

    Create principal.py

---
+++ principal.py
@@ -0,0 +1,11 @@
+# -- ------------------------------------------------------------------------------------ -- #
+
+# -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 2 - Behavioral Finance
+
+# -- archivo: datos.py - datos generales para uso en el proyecto
+
+# -- mantiene: marioabel96
+
+# -- repositorio: https://github.com/marioabel96/LAB_2_MAGV/
+
+# -- ------------------------------------------------------------------------------------ -- #
